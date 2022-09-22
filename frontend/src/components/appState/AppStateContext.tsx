02bba4ca61a7396ff3da4b3d777dc0f88600ddf2
git <baphemot@gmail.com>
2022-09-22 16:38:43 +0200

    Woo, s3 sync!

--- frontend/src/components/appState/AppStateContext.tsx
+++ frontend/src/components/appState/AppStateContext.tsx
@@ -1,161 +1,173 @@
-import React from 'react';
-import { encryptPayload, generateSalt } from '../../services/crypto';
-import { downloadBlob } from '../../services/download';
-import { Persist } from '../../services/persist';
-import { generateNewStore, generateNewStoreId, getStoreIdFromUrl, Store } from '../../services/store';
+import React from "react";
+import { encryptPayload, generateSalt } from "../../services/crypto";
+import { downloadBlob } from "../../services/download";
+import { Persist, PersistS3 } from "../../services/persist";
+import {
+  generateNewStore,
+  generateNewStoreId,
+  getStoreIdFromUrl,
+  Store,
+} from "../../services/store";
 
 type ContextType = {
-    editor: {
-        isActive: boolean,
-        setIsActive: (state: boolean) => void,
-    },
-    persistance: {
-        persistor: Persist | null,
-        store: () => void,
-    },
-    store: {
-        store: Store | null,
-        update: (field: keyof Store, value: string) => void,
-        setSalt: (salt: string) => void,
-        download: () => void,
-    }
-}
+  editor: {
+    isActive: boolean;
+    setIsActive: (state: boolean) => void;
+  };
+  persistance: {
+    persistor: Persist | null;
+    store: () => void;
+  };
+  store: {
+    store: Store | null;
+    update: (field: keyof Store, value: string) => void;
+    setSalt: (salt: string) => void;
+    download: () => void;
+  };
+};
 
 export const AppStateContext = React.createContext<ContextType>({
-    editor: {
-        isActive: false,
-        setIsActive: (state: boolean) => {},
-    },
-    persistance: {
-        persistor: null,
-        store: () => {},
-    },
-    store: {
-        store: null,
-        update: (field: keyof Store, value: string) => {},
-        setSalt: (salt: string) => {},
-        download: () => {},
-    }
-})
+  editor: {
+    isActive: false,
+    setIsActive: (state: boolean) => {},
+  },
+  persistance: {
+    persistor: null,
+    store: () => {},
+  },
+  store: {
+    store: null,
+    update: (field: keyof Store, value: string) => {},
+    setSalt: (salt: string) => {},
+    download: () => {},
+  },
+});
 
 type AppStatecontextProviderProps = {
-    children: React.ReactNode,
-}
+  children: React.ReactNode;
+};
 
-export const AppStateProvider = ({ children }: AppStatecontextProviderProps) => {
+export const AppStateProvider = ({
+  children,
+}: AppStatecontextProviderProps) => {
+  const [editor, setEditor] = React.useState({
+    isActive: false,
+  });
 
-    const [editor, setEditor] = React.useState({
-        isActive: false
-    })
-
-    const [persistor] = React.useState(() => {
-        const p = new Persist()
-        return p;
-    });    
-
-
-    const [store, setStore] = React.useState(generateNewStore());
-    const [salt, setSalt] = React.useState(window.location.hash.toString().substr(1));
-    const [storeId, setStoreId] = React.useState(() =>
-      getStoreIdFromUrl(window.location.pathname)
+  const [persistor] = React.useState(() => {
+    return new Persist(
+      new PersistS3(
+        "https://ns25oj2tt2.execute-api.eu-central-1.amazonaws.com/dev/upload",
+        "https://napkin-encrypted-storage-dev.s3.eu-central-1.amazonaws.com"
+      )
     );
-  
-  
-    React.useEffect(() => {
-      if (!storeId) {
-        setStoreId(generateNewStoreId());
-        setSalt(generateSalt());
-        setStore(generateNewStore());
-      } 
-      if (storeId && salt) {
-        window.history.pushState(undefined, "", `/n/${storeId}`);
-        window.location.hash = salt;
-      }
-    }, [storeId, salt, store]);
-  
-    React.useEffect(() => {
-      if (salt) {
-        window.location.hash = salt;
-      }
-    }, [salt])
-  
-    React.useEffect(() => {
-      if (store && storeId && salt) {
-        return persistor?.sync(store, storeId, salt);
-      } else {
-        persistor?.stop()
-      }
-    }, [store, storeId, salt, persistor])
-  
-    React.useEffect(() => {
-      if (storeId && salt) {
-        (async () => {
-          if (storeId && salt) {
-            try {
-                const remoteStore = await persistor?.retreiveData(storeId, salt);
-                if (remoteStore) {
-                    setStore(remoteStore)
-                } else {
-                    throw new Error()
-                }
-            } catch {
-                // TODO inform the UI that there was an error, do not try to override the store
-                persistor.stop();
-            }
-          }
-        })();
-      }
-    }, [storeId, salt, persistor])    
+  });
 
-    const storeNow = () => {
+  const [store, setStore] = React.useState(generateNewStore());
+  const [salt, setSalt] = React.useState(
+    window.location.hash.toString().substr(1)
+  );
+  const [storeId, setStoreId] = React.useState(() =>
+    getStoreIdFromUrl(window.location.pathname)
+  );
+
+  React.useEffect(() => {
+    if (!storeId) {
+      setStoreId(generateNewStoreId());
+      setSalt(generateSalt());
+      setStore(generateNewStore());
+    }
+    if (storeId && salt) {
+      window.history.pushState(undefined, "", `/n/${storeId}`);
+      window.location.hash = salt;
+    }
+  }, [storeId, salt, store]);
+
+  React.useEffect(() => {
+    if (salt) {
+      window.location.hash = salt;
+    }
+  }, [salt]);
+
+  React.useEffect(() => {
+    if (store && storeId && salt) {
+      return persistor?.sync(store, storeId, salt);
+    } else {
+      persistor?.stop();
+    }
+  }, [store, storeId, salt, persistor]);
+
+  React.useEffect(() => {
+    if (storeId && salt) {
+      (async () => {
         if (storeId && salt) {
-            return persistor?.syncNow(store, storeId, salt);
-        }
-    }
-
-    const updateStore = (field: keyof Store, value: string) => {
-        setStore({
-            ...store,
-            [field]: value
-        })
-    }
-
-    const changeSalt = (newSalt: string) => {
-        if (!storeId) return;
-        persistor.stop();
-        (async() => {
-            await persistor.storeData(store, storeId, newSalt);
-            setSalt(newSalt);
-        })()
-    }
-
-    return (
-        <AppStateContext.Provider value={{
-            editor: {
-                ...editor,
-                setIsActive: (state: boolean) => {
-                    setEditor({
-                        ...editor,
-                        isActive: state
-                    })
-                }
-            },
-            persistance: {
-                persistor,
-                store: storeNow,
-            },
-            store: {
-                store,
-                update: updateStore,
-                setSalt: changeSalt,
-                download: () => {
-                    (async() => {
-                        downloadBlob(JSON.stringify(store), 'napk.in.txt')
-                    })()
-                }
+          try {
+            const remoteStore = await persistor?.retreiveData(storeId, salt);
+            if (remoteStore) {
+              setStore(remoteStore);
+            } else {
+              throw new Error();
             }
-        }}>
-            {children}
-        </AppStateContext.Provider>
-    )
-}
+          } catch {
+            // TODO inform the UI that there was an error, do not try to override the store
+            persistor.stop();
+          }
+        }
+      })();
+    }
+  }, [storeId, salt, persistor]);
+
+  const storeNow = () => {
+    if (storeId && salt) {
+      return persistor?.syncNow(store, storeId, salt);
+    }
+  };
+
+  const updateStore = (field: keyof Store, value: string) => {
+    setStore({
+      ...store,
+      [field]: value,
+    });
+  };
+
+  const changeSalt = (newSalt: string) => {
+    if (!storeId) return;
+    persistor.stop();
+    (async () => {
+      await persistor.storeData(store, storeId, newSalt);
+      setSalt(newSalt);
+    })();
+  };
+
+  return (
+    <AppStateContext.Provider
+      value={{
+        editor: {
+          ...editor,
+          setIsActive: (state: boolean) => {
+            setEditor({
+              ...editor,
+              isActive: state,
+            });
+          },
+        },
+        persistance: {
+          persistor,
+          store: storeNow,
+        },
+        store: {
+          store,
+          update: updateStore,
+          setSalt: changeSalt,
+          download: () => {
+            (async () => {
+              downloadBlob(JSON.stringify(store), "napk.in.txt");
+            })();
+          },
+        },
+      }}
+    >
+      {children}
+    </AppStateContext.Provider>
+  );
+};
--- frontend/src/services/persist.ts
+++ frontend/src/services/persist.ts
@@ -9,103 +9,146 @@ export enum PersistorState {
 
 enum PersitorError {
   LOAD_ERROR,
-  SAVE_ERROR
+  SAVE_ERROR,
 }
 
 interface IPersistable {
-  persist: (data: string, storeId: string) => Promise<boolean>
-  retreive: (storeId: string) => Promise<string | null>
+  persist: (data: Uint8Array, storeId: string) => Promise<boolean>;
+  retreive: (storeId: string) => Promise<string | null>;
 }
 
-class PresistLocalStorage implements IPersistable {
-
-  persist = async (data: string, storeId: string) => {
-    window.localStorage.setItem(`store-${storeId}`, data);
+export class PresistLocalStorage implements IPersistable {
+  persist = async (data: Uint8Array, storeId: string) => {
+    window.localStorage.setItem(`store-${storeId}`, data.toString());
     return true;
-  }
-  
+  };
+
   retreive = async (storeId: string) => {
     return window.localStorage.getItem(`store-${storeId}`);
-  }  
+  };
 }
 
-type OnPersistorStateChangeCallback = (state: PersistorState) => void
+export class PersistS3 implements IPersistable {
+  storeUrl: string = "";
+  s3Url: string = "";
+
+  constructor(storeUrl: string, s3Url: string) {
+    this.storeUrl = storeUrl;
+    this.s3Url = s3Url;
+  }
+
+  persist = async (data: Uint8Array, storeId: string) => {
+    console.log("try parsist");
+    const shapeFileBlob = new Blob([data.toString()], {
+      type: "text/plain",
+    });
+
+    const file = new File([shapeFileBlob], `${storeId}.bin`, {
+      type: shapeFileBlob.type,
+    });
+
+    const formData = new FormData();
+    formData.set("file", file);
+
+    const request = await fetch(this.storeUrl, {
+      body: formData,
+      method: "POST",
+    });
+    const json = await request.json();
+    if (!json.link) {
+      throw new Error();
+    }
+
+    return true;
+  };
+
+  retreive = async (storeId: string) => {
+    const request = await fetch(`${this.s3Url}/${storeId}.bin`);
+    const response = request.text();
+    return response;
+  };
+}
+
+type OnPersistorStateChangeCallback = (state: PersistorState) => void;
 
 export class Persist {
+  constructor(implementation: IPersistable = new PresistLocalStorage()) {
+    this.implementation = implementation;
+  }
 
   syncTracker = 0;
   state = PersistorState.IDLE;
-  lastSync: Date | null = null
+  lastSync: Date | null = null;
 
-  implementation: IPersistable = new PresistLocalStorage()
+  implementation: IPersistable;
 
   callbacks: {
-    stateChange: Array<OnPersistorStateChangeCallback>
+    stateChange: Array<OnPersistorStateChangeCallback>;
   } = {
-    stateChange: []
-  }
+    stateChange: [],
+  };
 
   setState = (state: PersistorState) => {
     this.state = state;
-    this.callbacks.stateChange.forEach(callback => callback(this.state))    
-  }
+    this.callbacks.stateChange.forEach((callback) => callback(this.state));
+  };
 
-  retreiveData = async (storeId: string, salt: string): Promise<Store | null> => {
-    this.setState(PersistorState.LOADING)
+  retreiveData = async (
+    storeId: string,
+    salt: string
+  ): Promise<Store | null> => {
+    this.setState(PersistorState.LOADING);
     const store = await this.implementation.retreive(storeId);
     if (store) {
       try {
         const decrypted = await decryptPayload(store, salt);
-        this.setState(PersistorState.IDLE)
+        this.setState(PersistorState.IDLE);
         if (decrypted) {
           this.lastSync = new Date();
           return JSON.parse(decrypted);
         }
       } catch {
-        this.setState(PersistorState.IDLE)
+        this.setState(PersistorState.IDLE);
         throw new Error();
       }
     }
-    this.setState(PersistorState.IDLE)
+    this.setState(PersistorState.IDLE);
     throw new Error();
   };
 
-  storeData = async (
-    storeToPersist: Store,
-    storeId: string,
-    salt: string
-  ) => {
-    const payload = (await encryptPayload(storeToPersist, salt)).toString();
-    this.implementation.persist(payload, storeId);
-  };  
+  storeData = async (storeToPersist: Store, storeId: string, salt: string) => {
+    const payload = await encryptPayload(storeToPersist, salt);
+    await this.implementation.persist(payload as Uint8Array, storeId);
+  };
 
   syncNow = async (store: Store, storeId: string, salt: string) => {
-    this.setState(PersistorState.SAVING)
-    await this.storeData(store, storeId, salt)
-    setTimeout(() => {
-      this.setState(PersistorState.IDLE)
-      this.lastSync = new Date()
-    }, 1000)    
-  }
+    this.setState(PersistorState.SAVING);
+    await this.storeData(store, storeId, salt);
+    this.setState(PersistorState.IDLE);
+    this.lastSync = new Date();
+  };
 
   sync = (store: Store, storeId: string, salt: string) => {
+    return;
     window.clearInterval(this.syncTracker);
-    this.syncTracker = setInterval(async () => {  
-      this.syncNow(store, storeId, salt)
-    }, 5000) as unknown as number // TODO fixme
-  
+    this.syncTracker = setInterval(async () => {
+      this.syncNow(store, storeId, salt);
+    }, 5000) as unknown as number; // TODO fixme
+
     return () => window.clearInterval(this.syncTracker);
-  }
+  };
 
   stop = () => {
-    this.setState(PersistorState.IDLE)
+    this.setState(PersistorState.IDLE);
     window.clearInterval(this.syncTracker);
-  }
+  };
 
   onPersistorStateChange = (callback: OnPersistorStateChangeCallback) => {
-    this.callbacks.stateChange.push(callback)
-  }
+    this.callbacks.stateChange.push(callback);
+  };
   onPersistorStateChangeRemove = (callback: OnPersistorStateChangeCallback) => {
-    this.callbacks.stateChange = this.callbacks.stateChange.filter(cb => cb !== callback)
-  }
+    this.callbacks.stateChange = this.callbacks.stateChange.filter(
+      (cb) => cb !== callback
+    );
+  };
 }
