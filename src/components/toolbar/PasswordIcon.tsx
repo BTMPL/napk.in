f9546943d82729b2eb80023080259cf465ac7d2f
Bartosz Szczecinski <baphemot@gmail.com>
2022-09-22 12:53:45 +0200

    Add missing files, whoops

---
+++ src/components/toolbar/PasswordIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { ReactComponent as Icon } from './assets/password.svg';
+
+export const PasswordIcon = () => {
+    return (
+        <Icon style={{
+            width: '24px'
+        }} />
+    )
+}
