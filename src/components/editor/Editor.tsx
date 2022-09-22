11d62495f1f5ebfe5d4c535ef55a746ecb748c66
Bartosz Szczecinski <baphemot@gmail.com>
2022-09-22 11:22:13 +0200

    Fix newlines

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import MDEditor from '@uiw/react-md-editor';
 import { AppStateContext } from '../appState';
 
+import style from './style.module.css';
+
 type EditorProps = {
     onChange: (str: string) => void,
     value: string,
@@ -13,17 +15,19 @@ export const Editor = ({ onChange, value }: EditorProps) => {
     
 
     if (!isEditorActive) {
-        return <MDEditor.Markdown source={value} />
+        return (
+            <div className={style.editor}>
+                <MDEditor.Markdown source={value} />
+            </div>
+        );
     }
     return (
-        <>
-            <MDEditor
-                value={value}
-                onChange={(v) => onChange(v ?? '')}
-                preview={'edit'}
-                visibleDragbar={false}
-                height={'100%'}
-            />
-        </>
+        <MDEditor
+            value={value}
+            onChange={(v) => onChange(v ?? '')}
+            preview={'edit'}
+            visibleDragbar={false}
+            height={'100%'}
+        />
     )
 }
